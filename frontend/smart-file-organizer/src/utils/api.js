59e8cd394289e90d09ee9eeea27e4cd168fbbbdc
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-03-29 13:37:58 +0530

    e

--- frontend/smart-file-organizer/src/utils/api.js
+++ frontend/smart-file-organizer/src/utils/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
     baseURL: 'http://localhost:8080',
 })
 
-// Attach JWT to every request
 api.interceptors.request.use((config) => {
     const token = getToken()
     if (token) config.headers.Authorization = `Bearer ${token}`
