333b991a2a7f059ffa4d036e4b3eac4e452c660b
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-04-13 11:55:23 +0530

    added comment to productedroute

--- frontend/smart-file-organizer/src/components/ProtectedRoute.jsx
+++ frontend/smart-file-organizer/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { isLoggedIn } from '../utils/auth'
-
+//return true if i have jwt token valid
 export default function ProtectedRoute() {
     return isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />
 }
